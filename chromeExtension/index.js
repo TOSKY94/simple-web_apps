c04db47dbef5259e08a9a6b884532fc4287120c3
Patrick <32589922+TOSKY94@users.noreply.github.com>
2021-09-10 14:56:03 +0100

    modified

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,15 +1,29 @@
 
-let myLeads=["pascal","james"]
+let myLeads=[]
 const inputBtn = document.getElementById("input-btn")
 const inputTextEl = document.getElementById("input-el")
 const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputTextEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-for (let i=0; i<myLeads.length; i++){
-    console.log(myLeads[i])
-    ulEL.innerHTML+="<li>"+myLeads[i]+"</li>"
+
+function renderLeads(){
+    let listItems=""
+    for (let i=0; i<myLeads.length; i++){
+        listItems+=`
+            <li>
+                <a href='${myLeads[i]}' target='_blank'>
+                    ${myLeads[i]}
+                </a>
+            </li>`
+            
+        console.log(listItems)
+    }
+    ulEL.innerHTML = listItems
+    inputTextEl.value="";
 }
+
+//<li><a href='facebook.com' target='_blank'>facebook.com</a></li>
