b2b84b276f1a5c7240ff3d2923bd018620705eff
Patrick <32589922+TOSKY94@users.noreply.github.com>
2021-09-28 15:00:12 +0100

    modified

--- videoApp/index.js
+++ videoApp/index.js
@@ -1,21 +1,45 @@
 
-fetch('https://picsum.photos/800/600')
+fetch('https://picsum.photos/200/200')
     .then(res => res.blob())
     .then(blob => {
         let img = document.createElement('img');
         console.log(img)
         img.src = URL.createObjectURL(blob);
-        document.querySelector('body').appendChild(img)
+        document.querySelector('.image').appendChild(img)
     })
 
 
-    function factorial(num) {
-        if (num===0){
-            return 1
-        }  else {
-            return num*factorial(num-1);
-        }
-    }
 
-    let number = factorial(3);
-    console.log(number)
+
+const postSection =document.querySelector('#posts');
+const postTemplate =document.querySelector('#post-template');
+
+
+console.log('starting...');
+
+async function getData() {
+    const postStream = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postStream.json();
+    let i =0;
+
+    posts.forEach(post => {
+        i++;
+        
+        if (i<20){
+            const title=post.title;
+            const body=post.body;
+
+            const newPost = document.importNode(postTemplate.content, true );
+            const postTitle = newPost.querySelector('.post_title')
+            const postBody = newPost.querySelector('.post_body');
+
+            postTitle.innerText='Post#'+i+'\n'+title;
+            postBody.innerText=body;
+            postSection.appendChild(newPost)
+        }
+    })
+console.log('done...')
+}
+
+
+getData()
